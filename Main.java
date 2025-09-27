4229148b54fb23dbc430d98a76ab36f6d77b2ef3
TheDevIsHere <99741291+TheDevsIsHere@users.noreply.github.com>
2025-09-27 17:28:03 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,41 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Magazzino prodotti = new Magazzino();
+        Storage products = new Storage();
 
-        int scelta = 0, quantità = 0, exit = 0;
-        double prezzo = 0;
-        String nome, codice;
+        int choice = 0, quantity = 0, exit = 0;
+        double price = 0;
+        String name, code;
 
         while (exit != 1) {
-            System.out.println("1:aggiungi prodotti, 2.EliminaProdotto, 4:MostraTutti. 5:esci");
-            scelta = sc.nextInt();
-            switch (scelta) {
+            System.out.println("1: Add products, 2: Remove product, 4: Show all, 5: Exit");
+            choice = sc.nextInt();
+            switch (choice) {
                 case 1:
 
-                    System.out.println("Nome prodotto: ");
-                    nome = sc.next();
+                    System.out.println("Product name: ");
+                    name = sc.next();
 
-                    System.out.println("Quanti prodotti vuoi inserire: ");
-                    quantità = sc.nextInt();
+                    System.out.println("How many products do you want to add: ");
+                    quantity = sc.nextInt();
 
-                    System.out.println("Codice prodotto: ");
-                    codice = sc.next();
+                    System.out.println("Product code: ");
+                    code = sc.next();
 
-                    System.out.println("Prezzo: ");
-                    prezzo = sc.nextDouble();
+                    System.out.println("Price: ");
+                    price = sc.nextDouble();
 
-                    prodotti.AggiuntaProdotto(nome, codice, quantità, prezzo);
+                    products.addProduct(name, code, quantity, price);
                     break;
 
                 case 2:
-                    System.out.println("Codice prodotto da eliminare: ");
-                    codice = sc.next();
-                    prodotti.EliminaProdotto(codice);
+                    System.out.println("Enter the product code to remove: ");
+                    code = sc.next();
+                    products.removeProduct(code);
                     break;
 
                 case 4:
-                    prodotti.StampaProdotti();
+                    products.printProducts();
                     break;
 
                 case 5:
